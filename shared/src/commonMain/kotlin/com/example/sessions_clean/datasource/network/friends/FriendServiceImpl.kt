c705d34f16490e63d9197a47d431a49a82541813
Jonas Hiltl <jonashiltl2003@gmail.com>
2022-01-01 20:01:25 +0100

    remove unused imports

--- shared/src/commonMain/kotlin/com/example/sessions_clean/datasource/network/friends/FriendServiceImpl.kt
+++ shared/src/commonMain/kotlin/com/example/sessions_clean/datasource/network/friends/FriendServiceImpl.kt
@@ -2,10 +2,8 @@ package com.example.sessions_clean.datasource.network.friends
 
 import com.example.sessions_clean.model.Friend
 import com.example.sessions_clean.model.FriendList
-import com.example.sessions_clean.model.Profile
 import io.ktor.client.*
 import io.ktor.client.request.*
-import kotlin.collections.get
 
 class FriendServiceImpl(
     private val httpClient: HttpClient,
